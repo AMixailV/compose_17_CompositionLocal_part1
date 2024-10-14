4d26df9cffe86297cc3831ad9dbdeb1db9b62da3
AMixailV <Mixer-X@mail.ru>
2024-10-14 20:19:56 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "compose_17_CompositionLocal_part1"
+include(":app")
+ 
